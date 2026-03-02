f2060f786e3936915f227fac35727f1aa1408df5
Jayesh Gupta <jayeshgupta.997@gmail.com>
2026-03-03 03:26:47 +0530

    International section added to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,17 @@ import Hero from "./Hero";
 import Experience from "./Experience";
 import Destinations from "./Destinations";
 import AboutUs from "./AboutUs";
+import India from "./India";
+import International from "./International";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Experience />
-      <Destinations />
+      {/* <Destinations /> */}
+      <India />
+      <International />
       <AboutUs />
     </>
   );
